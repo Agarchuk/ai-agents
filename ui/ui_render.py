99f660aa18bf2d5a4edb1ffd2deb354472538326
Hanna Harchuk <harchukhanna@gmail.com>
2025-03-15 18:06:46 +0200

    add auth0

--- ui/ui_render.py
+++ ui/ui_render.py
@@ -1,10 +1,19 @@
 import streamlit as st
 
+from ui.pages.login import LoginPage
+from ui.utils.session_config import SessionConfig
+from ui.utils.session_state_service import SessionStateService
+
 class App:
     def render(self):  
-        pages = [
-            st.Page("ui/pages/0_home.py", title="Home", icon=":material/home:"),
-        ]
+        session_token =  SessionStateService().get(SessionConfig.TOKEN_KEY, None) 
+
+        if session_token is None:
+            LoginPage().display()
+        else:
+            pages = [
+                st.Page("ui/pages/0_home.py", title="Home", icon=":material/home:"),
+            ]
                 
-        pg = st.navigation(pages)
-        pg.run()
+            pg = st.navigation(pages)
+            pg.run()
