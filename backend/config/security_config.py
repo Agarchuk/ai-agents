039658c4b692ed1619a67bbbcd581f4478b4148b
Hanna Harchuk <harchukhanna@gmail.com>
2025-03-15 19:00:10 +0200

    fix config

--- backend/config/security_config.py
+++ backend/config/security_config.py
@@ -3,11 +3,11 @@ from dotenv import load_dotenv
 import streamlit as st
 load_dotenv()
 
-AUTH0_CLIENT_ID = os.getenv("AUTH0_CLIENT_ID", st.secrets["AUTH0_CLIENT_ID"])
-AUTH0_CLIENT_SECRET = os.getenv("AUTH0_CLIENT_SECRET", st.secrets["AUTH0_CLIENT_SECRET"])
-AUTH0_DOMAIN = os.getenv("AUTH0_DOMAIN", st.secrets["AUTH0_DOMAIN"])
-AUTH0_AUDIENCE = os.getenv("AUTH0_AUDIENCE", f"https://{AUTH0_DOMAIN}/userinfo")
+AUTH0_CLIENT_ID = st.secrets["auth0"]["AUTH0_CLIENT_ID"]
+AUTH0_CLIENT_SECRET = st.secrets["auth0"]["AUTH0_CLIENT_SECRET"]
+AUTH0_DOMAIN = st.secrets["auth0"]["AUTH0_DOMAIN"]
+AUTH0_AUDIENCE = f"https://{AUTH0_DOMAIN}/userinfo"
 AUTH0_AUTHORIZATION_URL = f"https://{AUTH0_DOMAIN}/authorize"
 AUTH0_TOKEN_URL = f"https://{AUTH0_DOMAIN}/oauth/token"
 AUTH0_REVOKE_URL = f"https://{AUTH0_DOMAIN}/revoke"
-AUTH0_REDIRECT_URI = os.getenv("AUTH0_REDIRECT_URI", st.secrets["AUTH0_REDIRECT_URI"])
+AUTH0_REDIRECT_URI = st.secrets["auth0"]["AUTH0_CALLBACK_URL"]
