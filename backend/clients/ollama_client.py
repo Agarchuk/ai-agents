8cd353dbfd2a27303b7628e587b97fc91eb5ff1b
Hanna Harchuk <harchukhanna@gmail.com>
2025-03-16 14:54:50 +0200

    Implement OllamaClient
    * Implement OllamaClient for model retrieval and request handling; add configuration for API URL and integrate with home page UI.

---
+++ backend/clients/ollama_client.py
@@ -0,0 +1,38 @@
+from typing import Any, Dict, Tuple
+import json
+
+import requests
+from backend.config.ollama_config import OLLAMA_API_URL
+from utils.logger import log_info
+from typing import List
+
+class OllamaClient:
+
+    def get_models(self) -> List[str]:
+        response = requests.get(f"{OLLAMA_API_URL}/api/tags")
+        response.raise_for_status()
+        log_info(f"Models: {response.json()}")
+        return response.json()
+
+    def send_request(self, system_prompt: str, user_prompt: str) -> Tuple[Dict[str, Any], float]:
+        payload = {
+            "model": "smollm2:135m",
+            "system": system_prompt,
+            "prompt": user_prompt,
+            "stream": False
+        }
+        try:
+            response = requests.post(f"{OLLAMA_API_URL}/api/generate", json=payload)
+            response.raise_for_status()
+            
+            log_info(f"Response: {response}")
+            return response
+            
+        except requests.exceptions.RequestException as e:
+            return {"error": f"HTTP error: {str(e)}"}
+        except json.JSONDecodeError:
+            return {"error": "Invalid JSON response"}
+        except Exception as e:
+            return {"error": f"Unexpected error: {str(e)}"}
+
+    
---
+++ backend/config/ollama_config.py
@@ -0,0 +1,3 @@
+import streamlit as st
+
+OLLAMA_API_URL = st.secrets["ollama"]["OLLAMA_API_URL"]
